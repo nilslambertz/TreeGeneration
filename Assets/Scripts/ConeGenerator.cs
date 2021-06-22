c83593bb32273e425ffdd296e9e164b757d0b894
nilslambertz <nilslambertz@web.de>
2021-06-22 18:14:27 +0200

    simplified code

--- Assets/Scripts/ConeGenerator.cs
+++ Assets/Scripts/ConeGenerator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ConeGenerator : MonoBehaviour
 {
@@ -12,6 +9,7 @@ public class ConeGenerator : MonoBehaviour
 	private float bottomRadius = 10;
 	private float topRadius = 5;
 	private float height = 20;
+	private int numberOfSides = 62;
 
 	public void Start()
 	{
@@ -22,32 +20,27 @@ public class ConeGenerator : MonoBehaviour
 		Mesh mesh = filter.mesh;
 		mesh.Clear();
 
-		int nbSides = 18;
-		int nbHeightSeg = 1; // Not implemented yet
-
-		int nbVerticesCap = nbSides + 1;
-
-		#region Vertices
+		int nbVerticesCap = numberOfSides + 1;
 
 		// bottom + top + sides
-		Vector3[] vertices = new Vector3[nbVerticesCap + nbVerticesCap + nbSides * nbHeightSeg * 2 + 2];
+		vertices = new Vector3[(numberOfSides + 1) * 4];
 		int vert = 0;
 		float _2pi = Mathf.PI * 2f;
 
 		// Bottom cap
 		vertices[vert++] = new Vector3(0f, 0f, 0f);
-		while (vert <= nbSides)
+		while (vert <= numberOfSides)
 		{
-			float rad = (float) vert / nbSides * _2pi;
+			float rad = (float) vert / numberOfSides * _2pi;
 			vertices[vert] = new Vector3(Mathf.Cos(rad) * bottomRadius, 0f, Mathf.Sin(rad) * bottomRadius);
 			vert++;
 		}
 
 		// Top cap
 		vertices[vert++] = new Vector3(0f, height, 0f);
-		while (vert <= nbSides * 2 + 1)
+		while (vert <= numberOfSides * 2 + 1)
 		{
-			float rad = (float) (vert - nbSides - 1) / nbSides * _2pi;
+			float rad = (float) (vert - numberOfSides - 1) / numberOfSides * _2pi;
 			vertices[vert] = new Vector3(Mathf.Cos(rad) * topRadius, height, Mathf.Sin(rad) * topRadius);
 			vert++;
 		}
@@ -56,32 +49,30 @@ public class ConeGenerator : MonoBehaviour
 		int v = 0;
 		while (vert <= vertices.Length - 4)
 		{
-			float rad = (float) v / nbSides * _2pi;
+			float rad = (float) v / numberOfSides * _2pi;
 			vertices[vert] = new Vector3(Mathf.Cos(rad) * topRadius, height, Mathf.Sin(rad) * topRadius);
 			vertices[vert + 1] = new Vector3(Mathf.Cos(rad) * bottomRadius, 0, Mathf.Sin(rad) * bottomRadius);
 			vert += 2;
 			v++;
 		}
 
-		vertices[vert] = vertices[nbSides * 2 + 2];
-		vertices[vert + 1] = vertices[nbSides * 2 + 3];
-
-		#endregion
-
-		#region Normales
+		vertices[vert] = vertices[numberOfSides * 2 + 2];
+		vertices[vert + 1] = vertices[numberOfSides * 2 + 3];
+		
+		/* Normals */
 
 		// bottom + top + sides
 		Vector3[] normales = new Vector3[vertices.Length];
 		vert = 0;
 
 		// Bottom cap
-		while (vert <= nbSides)
+		while (vert <= numberOfSides)
 		{
 			normales[vert++] = Vector3.down;
 		}
 
 		// Top cap
-		while (vert <= nbSides * 2 + 1)
+		while (vert <= numberOfSides * 2 + 1)
 		{
 			normales[vert++] = Vector3.up;
 		}
@@ -90,7 +81,7 @@ public class ConeGenerator : MonoBehaviour
 		v = 0;
 		while (vert <= vertices.Length - 4)
 		{
-			float rad = (float) v / nbSides * _2pi;
+			float rad = (float) v / numberOfSides * _2pi;
 			float cos = Mathf.Cos(rad);
 			float sin = Mathf.Sin(rad);
 
@@ -101,30 +92,26 @@ public class ConeGenerator : MonoBehaviour
 			v++;
 		}
 
-		normales[vert] = normales[nbSides * 2 + 2];
-		normales[vert + 1] = normales[nbSides * 2 + 3];
-
-		#endregion
-
-		#region UVs
+		normales[vert] = normales[numberOfSides * 2 + 2];
+		normales[vert + 1] = normales[numberOfSides * 2 + 3];
 
 		Vector2[] uvs = new Vector2[vertices.Length];
 
 		// Bottom cap
 		int u = 0;
 		uvs[u++] = new Vector2(0.5f, 0.5f);
-		while (u <= nbSides)
+		while (u <= numberOfSides)
 		{
-			float rad = (float) u / nbSides * _2pi;
+			float rad = (float) u / numberOfSides * _2pi;
 			uvs[u] = new Vector2(Mathf.Cos(rad) * .5f + .5f, Mathf.Sin(rad) * .5f + .5f);
 			u++;
 		}
 
 		// Top cap
 		uvs[u++] = new Vector2(0.5f, 0.5f);
-		while (u <= nbSides * 2 + 1)
+		while (u <= numberOfSides * 2 + 1)
 		{
-			float rad = (float) u / nbSides * _2pi;
+			float rad = (float) u / numberOfSides * _2pi;
 			uvs[u] = new Vector2(Mathf.Cos(rad) * .5f + .5f, Mathf.Sin(rad) * .5f + .5f);
 			u++;
 		}
@@ -133,7 +120,7 @@ public class ConeGenerator : MonoBehaviour
 		int u_sides = 0;
 		while (u <= uvs.Length - 4)
 		{
-			float t = (float) u_sides / nbSides;
+			float t = (float) u_sides / numberOfSides;
 			uvs[u] = new Vector3(t, 1f);
 			uvs[u + 1] = new Vector3(t, 0f);
 			u += 2;
@@ -143,17 +130,15 @@ public class ConeGenerator : MonoBehaviour
 		uvs[u] = new Vector2(1f, 1f);
 		uvs[u + 1] = new Vector2(1f, 0f);
 
-		#endregion
+		/* Triangles */
 
-		#region Triangles
-
-		int nbTriangles = nbSides + nbSides + nbSides * 2;
-		int[] triangles = new int[nbTriangles * 3 + 3];
+		int nbTriangles = numberOfSides + numberOfSides + numberOfSides * 2;
+		triangles = new int[nbTriangles * 3 + 3];
 
 		// Bottom cap
 		int tri = 0;
 		int i = 0;
-		while (tri < nbSides - 1)
+		while (tri < numberOfSides - 1)
 		{
 			triangles[i] = 0;
 			triangles[i + 1] = tri + 1;
@@ -170,7 +155,7 @@ public class ConeGenerator : MonoBehaviour
 
 		// Top cap
 		//tri++;
-		while (tri < nbSides * 2)
+		while (tri < numberOfSides * 2)
 		{
 			triangles[i] = tri + 2;
 			triangles[i + 1] = tri + 1;
@@ -201,8 +186,6 @@ public class ConeGenerator : MonoBehaviour
 			tri++;
 			i += 3;
 		}
-
-		#endregion
 
 		mesh.vertices = vertices;
 		mesh.normals = normales;
