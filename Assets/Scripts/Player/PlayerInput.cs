78b9c447254ed7a6f20230332ca16dfa1d1ad390
nilslambertz <nilslambertz@web.de>
2021-08-14 16:42:10 +0200

    added PlayerInput-script, which handles all inputs except movement, circle is now drawn on the ground where the player is looking and a tree is spawned there when pressing left mouse button

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+    public Transform position;
+    public LayerMask mask;
+
+    private GameObject circle;
+    private bool circleHidden = true;
+
+    private void Start() {
+        circle =  GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        circle.transform.localScale = new Vector3(1, 0.1f, 1);
+        circle.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Physics.Raycast(position.position, position.forward, out var hit, Mathf.Infinity, mask)) {
+            if (circleHidden) {
+                circle.SetActive(true);
+            }
+            
+            circle.transform.position = hit.point;
+        }
+        else {
+            circleHidden = true;
+            circle.SetActive(false);
+        };
+        
+        if (Input.GetMouseButtonDown(0)) {
+            GeneratorScript.startWeber(hit.point);
+        }
+    }
+}
