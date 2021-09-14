50de1d07e2838cad8690531e2937ba49b058cf11
nilslambertz <nilslambertz@web.de>
2021-09-14 16:07:53 +0200

    added script for OptionList

---
+++ Assets/Scripts/UI/OptionListScript.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class OptionListScript : MonoBehaviour
+    {
+        public GameObject optionsElementTemplate;
+
+        public GameObject optionListPanel;
+
+        private static OptionElementInfo[] optionList;
+
+        public enum OptionElement
+        {
+            showDebug,
+            roundedBranches,
+            animationRendering
+        }
+
+        public struct OptionElementInfo
+        {
+            public OptionElementInfo(string n, string d, bool v)
+            {
+                name = n;
+                description = d;
+                value = v;
+            }
+            public string name;
+            public string description;
+            public bool value;
+            public void changeValue(bool newValue)
+            {
+                value = newValue;
+            }
+        }
+
+        private static void generateOptionList()
+        {
+            optionList = new OptionElementInfo[3];
+            optionList[(int)OptionElement.showDebug] = new OptionElementInfo("Debug output", "Show debug information in the top left corner", true);
+            optionList[(int)OptionElement.roundedBranches] = new OptionElementInfo("Rounded branches", "Render curved branches (decreases performance dramatically!)", false);
+            optionList[(int)OptionElement.animationRendering] = new OptionElementInfo("Animate generation", "Animate the generation of trees", true);
+        }
+
+        public static OptionElementInfo getOption(OptionElement o)
+        {
+            if (optionList == null)
+            {
+                generateOptionList();
+            }
+            return optionList[(int)o];
+        }
+
+        void Start()
+        {
+            generateOptionList();
+            updateOptionsList();
+        }
+
+        void updateOptionsList()
+        {
+            foreach (Transform child in optionListPanel.transform)
+            {
+                Destroy(child.gameObject);
+            }
+
+            GameObject currentOption;
+            for (int i = 0; i < optionList.Length; i++)
+            {
+                OptionElementInfo o = optionList[i];
+
+                currentOption = Instantiate(optionsElementTemplate, optionListPanel.transform);
+                currentOption.transform.Find("Title").GetComponent<Text>().text = o.name;
+                currentOption.transform.Find("Description").GetComponent<Text>().text = o.description;
+
+                Toggle toggle = currentOption.transform.Find("Toggle").GetComponent<Toggle>();
+                toggle.isOn = o.value;
+
+                int x = i;
+                toggle.onValueChanged.AddListener(delegate (bool newValue)
+                {
+                    optionList[x].changeValue(newValue);
+                });
+            }
+        }
+    }
+}
